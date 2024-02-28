d8dc9f3549c54ba1d0fb1f3fad057120d09b5cef
mqurban <mianqurban525@gmail.com>
2024-02-28 10:47:33 +0500

    javascript class1

---
+++ class1/index.js
@@ -0,0 +1,41 @@
+// console.log(2+2);
+// document.write("hey there! ");
+// alert("helo");
+
+
+// Data Types 
+
+// integers = 1,2,3,4,5,6,7,8,9,10;
+// float = 1.2, 3.4 , 5.6, 7.8, 9.10;
+// string = "hello", "world", 'hey there';
+// bolean = true, false;
+// undefined = undefined;
+// null = null;
+// array = [1,2,3,4,5,6,7,8,9,10];
+
+// array[0]= 1;
+// array[1]= 2;
+
+// Variable 
+
+// var, let, const 
+
+ 
+
+ // 20;
+//  var age = 30;
+//  age = 40;
+
+//  alert(age);
+
+
+//  const pi = 3.14;
+//  pi = 3.15;
+//     alert(pi);
+
+
+// let @variable1 = 30;
+// let first-variable = "helo";
+// let #second = "helo";
+// let &third = "helo";
+// let /fourth = "helo";
