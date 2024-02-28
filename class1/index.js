4630d6e0f241b0722b97a5055652366fd5d74388
mqurban <mianqurban525@gmail.com>
2024-02-28 22:03:44 +0500

    javascript class 2

--- class1/index.js
+++ class1/index.js
@@ -39,3 +39,5 @@
 // let #second = "helo";
 // let &third = "helo";
 // let /fourth = "helo";
+
+
---
+++ class2/index.js
@@ -0,0 +1,104 @@
+
+// Operators
+
+// arthimatic operatos
+ 
+// +, -, *, /, %, ++, --
+
+// let a1 = 10; 
+// let a2 = 20; 
+
+// let result = a1 + a2;
+// let result = a1 - a2;
+// let result  = a1 * a2;
+// let result = a1 / a2;
+// let result = a1 % a2;
+// a1++;
+// a2--;
+// console.log(a1);
+// console.log(a2);
+
+// console.log(result);
+
+
+// comparsion operators
+
+// ==, === , != !==, >, <, >=, <=
+
+// let a1 = "10"; 
+// let a2 = 10; 
+
+// console.log(a1 == a2);
+
+// console.log(a1 === a2);
+
+// let, var, const 
+
+
+// let a1 = 10
+// let a1 = 10
+
+// var a2 = 20; 
+// var a2 = 30;
+
+// const pi = 3.14;
+// const pi = 3.23;
+
+// let a1 = '20'; 
+// let a2 = 20; 
+
+// console.log(a1 != a2);
+
+
+// let a1 = 10;
+// let a2 = 20; 
+
+// console.log(a1 < a2);
+
+
+// Logical Operators
+
+// && || !
+
+// let num1 = false;
+// let num2 = false;
+
+// console.log(2==2 && 2==2);
+
+// console.log(num1 || num2);
+
+// console.log( !num1 );
+
+
+
+
+ //  type casting
+//  let a1 = 10 
+//  let a2 = "20abc";
+ 
+//  console.log(a1 + Number(a2));
+ 
+
+//  console.log(a1 + parseInt(a2));
+
+
+// console.log(a1 + parseFloat(a2));
+
+
+// if else if else/
+
+
+let a1 = parseInt(window.prompt("Enter number1: "));
+let a2 = parseInt(window.prompt("Enter number2: "));
+
+// console.log(a1 + a2);
+
+if (a1+ a2 > 100){
+    alert("Sum is greater than 100")
+}
+else if(a1 + a2 < 100){
+    alert("Sum is less than 100")
+}
+else {
+    alert("Sum is equal to 100")
+}
