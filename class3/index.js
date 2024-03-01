2d59239436c03cd54417bb4120d07549f8bda26a
mqurban <mianqurban525@gmail.com>
2024-03-01 10:39:35 +0500

    third class uploaded

---
+++ class3/index.js
@@ -0,0 +1,56 @@
+//Loops 
+// for loop 
+// while loop 
+// do while loop 
+
+// For Loop  -  for (initialization; condition; increment/decrement) { // code to be executed }
+
+// for (let i = 0; i <= 10; i+=2) {
+//     console.log("hello world")
+// }
+
+// let k = 0; 
+// while(1 == 1) {
+//     console.log("hello world")
+//     // k++;
+// }
+
+// j = 0;
+// do {
+//     console.log("hello world")
+//     j++;
+// } while( 0 < 0) 
+
+
+// for (let i = 10; i >= 0; i-- ){
+//     console.log(i);
+// }
+
+// Functions 
+// function myfirstfunction() {
+//     alert("Hello World");
+// }
+
+// myfirstfunction();
+// myfirstfunction();
+// myfirstfunction();
+
+// function mysecondfunction(){
+//     let num1 = 10;
+//     let num2 = 20;
+//     let result = num1 + num2;
+//     return result;
+// }
+
+// let output = mysecondfunction();
+
+// let result2 = output +10;
+
+// alert(result2);
+
+// function mythirdfunction(name, age, class1) {
+//     // alert("Hello " + name + " your age is " + age);
+//     alert(`Hello ${name} your age is ${age} and in class ${class1}`); 
+// }1
+
+// mythirdfunction("Qurban", 25, "Web Development");
